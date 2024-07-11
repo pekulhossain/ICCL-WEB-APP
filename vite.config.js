33b03cd97a65fc4fbd3ddf82a5cb57e09ce1c0ca
pekulhossain <pekulhossain@gmail.com>
2024-07-12 00:12:17 +0600

    add

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  devServer: {
+    bonjour: true,
+  },
   plugins: [react()],
   assetsInclude: ['**/*.JPG']
 })
+
 
 
