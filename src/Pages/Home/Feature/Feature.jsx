ac8c4f96dc972ee66728e85eecf48dd556d82c8b
pekulhossain <pekulhossain@gmail.com>
2024-08-06 13:17:16 +0600

    updated

--- src/Pages/Home/Feature/Feature.jsx
+++ src/Pages/Home/Feature/Feature.jsx
@@ -1,74 +1,71 @@
+import React from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { GiSewingMachine } from "react-icons/gi";
-// motion
-import {motion} from 'framer-motion';
-//variants
-import {fadeIn} from '../../../FramerMotion/variants'
-const Feature = () => {
+import { motion } from "framer-motion";
+import { fadeIn } from '../../../FramerMotion/variants';
 
+const Feature = () => {
     return (
-        <section 
-       
-        className=" bg-slate-100 pt-16 mb-12">
+        <section className="bg-slate-100 pt-16 mb-12">
             <motion.div
-             variants={fadeIn("up", 0.8)}
-             initial ="hidden"
-             whileInView={"show"}
-             viewport={{once:false, amount: 0.7}}
+                variants={fadeIn("up", 0.8)}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false, amount: 0.7 }}
             >
-            <SectionTitle subHeading={"Our Features"}
-                heading={"We Always Try To Understand Users Expectation"}>
-            </SectionTitle>
-            <div>
-                <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, asperiores.</p>
-            </div>
+                <SectionTitle subHeading={"Our Features"} heading={"We Always Try To Understand Users Expectation"} />
+                <div>
+                    <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, asperiores.</p>
+                </div>
             </motion.div>
 
-            <div className="grid items-center justify-items-center gap-4 md:grid-cols-2 lg:grid-cols-3 mt-8 pt-12 pb-16">
-                    <motion.div
-                     variants={fadeIn("right", )}
-                     initial ="hidden"
-                     whileInView={"show"}
-                     viewport={{once:false, amount: 0.7}}
-                    className="card w-96 h-64 bg-white shadow-xl transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-95">
-                        <div className="card-body ">
-                            <div className=" w-8 justify-items-center text-center">
-                                <GiSewingMachine className=" flex justify-items-center  items-center  items-center text-pink-500 text-7xl align-middle"></GiSewingMachine>
-                            </div>
-                            <h2 className="card-title ">MANUFACTURING PRECISION</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, reiciendis!</p>
+            <div className="grid items-center justify-items-center gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 pt-12 pb-16">
+                <motion.div
+                    variants={fadeIn("up")}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: false, amount: 0.7 }}
+                    className="card w-full sm:w-80 md:w-96 h-64 bg-white shadow-xl transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-95"
+                >
+                    <div className="card-body">
+                        <div className="w-8 justify-items-center text-center">
+                            <GiSewingMachine className="flex justify-items-center items-center text-pink-500 text-7xl align-middle" />
                         </div>
-                    </motion.div>
-                    <motion.div
-                     variants={fadeIn("up", 0.6)}
-                     initial ="hidden"
-                     whileInView={"show"}
-                     viewport={{once:false, amount: 0.7}}
-                    className="card w-96 h-64 bg-white shadow-xl transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-95 ">
-                        <div className="card-body ">
-                            <div className=" w-8 justify-items-center text-center">
-                                <GiSewingMachine className=" flex justify-items-center  items-center  items-center text-pink-500 text-7xl align-middle"></GiSewingMachine>
-                            </div>
-                            <h2 className="card-title ">ACCLAIMED CLIENTS</h2>
-                            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, reiciendis!</p>
+                        <h2 className="card-title">MANUFACTURING PRECISION</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, reiciendis!</p>
+                    </div>
+                </motion.div>
+                <motion.div
+                    variants={fadeIn("up", 0.6)}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: false, amount: 0.7 }}
+                    className="card w-full sm:w-80 md:w-96 h-64 bg-white shadow-xl transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-95"
+                >
+                    <div className="card-body">
+                        <div className="w-8 justify-items-center text-center">
+                            <GiSewingMachine className="flex justify-items-center items-center text-pink-500 text-7xl align-middle" />
                         </div>
-                    </motion.div>
-                    <motion.div 
-                     variants={fadeIn("left",)}
-                     initial ="hidden"
-                     whileInView={"show"}
-                     viewport={{once:false, amount: 0.7}}
-                    className="card w-96 h-64 bg-white shadow-xl transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-95 ">
-                        <div className="card-body ">
-                            <div className=" w-8 justify-items-center text-center">
-                                <GiSewingMachine className=" flex justify-items-center  items-center  items-center text-pink-500 text-7xl align-middle"></GiSewingMachine>
-                            </div>
-                            <h2 className="card-title ">QUALITY ENSURED</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, reiciendis!</p>
+                        <h2 className="card-title">ACCLAIMED CLIENTS</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, reiciendis!</p>
+                    </div>
+                </motion.div>
+                <motion.div
+                    variants={fadeIn("up")}
+                    initial="hidden"
+                    whileInView="show"
+                    viewport={{ once: false, amount: 0.7 }}
+                    className="card w-full sm:w-80 md:w-96 h-64 bg-white shadow-xl transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-95"
+                >
+                    <div className="card-body">
+                        <div className="w-8 justify-items-center text-center">
+                            <GiSewingMachine className="flex justify-items-center items-center text-pink-500 text-7xl align-middle" />
                         </div>
-                    </motion.div>
-                    
-                </div>
+                        <h2 className="card-title">QUALITY ENSURED</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, reiciendis!</p>
+                    </div>
+                </motion.div>
+            </div>
         </section>
     );
 };
--- src/Pages/Home/Servicess/Service.jsx
+++ src/Pages/Home/Servicess/Service.jsx
@@ -1,74 +1,60 @@
+import React from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import knittingimg from '../../../assets/Services/knitting.JPG';
 import dyeingimg from '../../../assets/Services/dyeing.JPG';
 import printing from '../../../assets/Services/printing.JPG';
 import Counter from '../../../Counteranimation/Counter';
 import { Helmet } from "react-helmet-async";
-// import embroidaryimg from '../../../assets/Services/emborydary.JPG';
-// import garmentimg from '../../../assets/Services/garment.JPG';
-// import aopimg from '../../../assets/Services/aop.JPG';
+
 const Service = () => {
     return (
         <section className="py-28">
             <Helmet>
                 <title>ICCL || Services</title>
             </Helmet>
-            <SectionTitle subHeading={'WHAT WE DO'}
-                heading={'Our Services'}>
+            <SectionTitle subHeading={'WHAT WE DO'} heading={'Our Services'} />
 
-            </SectionTitle>
-
-            <div className="items-center justify-items-center grid gap-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-                <div className="card transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-105 w-96 bg-base-100 shadow-xl">
-                    <figure><img src={knittingimg} alt="" /></figure>
-                    <div className="card-body">
-                        {/* <h2 className="card-title">2,125Ton/Month</h2> */}
-                        <p className="card-title text-2xl"><Counter number={1523} title=" Ton/Month" /></p>
-                        <p className=" font-semibold">Knitting</p>
-                        
+            <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-items-center">
+                <div className="card transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-105 w-full sm:w-80 md:w-96 bg-base-100 shadow-xl">
+                    <figure><img src={knittingimg} alt="Knitting" className="w-full h-48 object-cover" /></figure>
+                    <div className="card-body p-4 md:p-6 lg:p-8">
+                        <p className="card-title text-xl md:text-2xl lg:text-3xl"><Counter number={1523} title=" Ton/Month" /></p>
+                        <p className="font-semibold text-sm md:text-base lg:text-lg">Knitting</p>
                     </div>
                 </div>
-                <div className="card transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-105 w-96 bg-base-100 shadow-xl">
-                    <figure><img src={dyeingimg} alt="" /></figure>
-                    <div className="card-body">
-                        {/* <h2 className="card-title">2,400Ton/Month</h2> */}
-                        <p className="card-title text-2xl"><Counter number={1324} title=" Ton/Month" /></p>
-                        <p className=" font-semibold">Dyeing</p>
+                <div className="card transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-105 w-full sm:w-80 md:w-96 bg-base-100 shadow-xl">
+                    <figure><img src={dyeingimg} alt="Dyeing" className="w-full h-48 object-cover" /></figure>
+                    <div className="card-body p-4 md:p-6 lg:p-8">
+                        <p className="card-title text-xl md:text-2xl lg:text-3xl"><Counter number={1324} title=" Ton/Month" /></p>
+                        <p className="font-semibold text-sm md:text-base lg:text-lg">Dyeing</p>
                     </div>
                 </div>
-                <div className="card transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-105 w-96 bg-base-100 shadow-xl">
-                    <figure><img src={printing} alt="" /></figure>
-                    <div className="card-body">
-                        {/* <h2 className="card-title">3.5Million Pcs/Month</h2>
-                        <p>Screen Printing</p> */}
-                        <p className="card-title text-2xl"><Counter number={100000} title=" Pcs/Month" /></p>
-                        <p className=" font-semibold">Screen Printing</p>
+                <div className="card transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-105 w-full sm:w-80 md:w-96 bg-base-100 shadow-xl">
+                    <figure><img src={printing} alt="Screen Printing" className="w-full h-48 object-cover" /></figure>
+                    <div className="card-body p-4 md:p-6 lg:p-8">
+                        <p className="card-title text-xl md:text-2xl lg:text-3xl"><Counter number={100000} title=" Pcs/Month" /></p>
+                        <p className="font-semibold text-sm md:text-base lg:text-lg">Screen Printing</p>
                     </div>
                 </div>
-                <div className="card transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-105 w-96 bg-base-100 shadow-xl">
-                    <figure><img src={knittingimg} alt="" /></figure>
-                    <div className="card-body">
-                        {/* <h2 className="card-title">2,125Ton/Month</h2> */}
-                        <p className="card-title text-2xl"><Counter number={1523} title=" Ton/Month" /></p>
-                        <p className=" font-semibold">Knitting</p>
-                        
+                <div className="card transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-105 w-full sm:w-80 md:w-96 bg-base-100 shadow-xl">
+                    <figure><img src={knittingimg} alt="Knitting" className="w-full h-48 object-cover" /></figure>
+                    <div className="card-body p-4 md:p-6 lg:p-8">
+                        <p className="card-title text-xl md:text-2xl lg:text-3xl"><Counter number={1523} title=" Ton/Month" /></p>
+                        <p className="font-semibold text-sm md:text-base lg:text-lg">Knitting</p>
                     </div>
                 </div>
-                <div className="card transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-105 w-96 bg-base-100 shadow-xl">
-                    <figure><img src={dyeingimg} alt="" /></figure>
-                    <div className="card-body">
-                        {/* <h2 className="card-title">2,400Ton/Month</h2> */}
-                        <p className="card-title text-2xl"><Counter number={1324} title=" Ton/Month" /></p>
-                        <p className=" font-semibold">Dyeing</p>
+                <div className="card transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-105 w-full sm:w-80 md:w-96 bg-base-100 shadow-xl">
+                    <figure><img src={dyeingimg} alt="Dyeing" className="w-full h-48 object-cover" /></figure>
+                    <div className="card-body p-4 md:p-6 lg:p-8">
+                        <p className="card-title text-xl md:text-2xl lg:text-3xl"><Counter number={1324} title=" Ton/Month" /></p>
+                        <p className="font-semibold text-sm md:text-base lg:text-lg">Dyeing</p>
                     </div>
                 </div>
-                <div className="card transform hover:bg-pink-500 hover:text-white trasition duration-500 hover:scale-105 w-96 bg-base-100 shadow-xl">
-                    <figure><img src={printing} alt="" /></figure>
-                    <div className="card-body">
-                        {/* <h2 className="card-title">3.5Million Pcs/Month</h2>
-                        <p>Screen Printing</p> */}
-                        <p className="card-title text-2xl"><Counter number={100000} title=" Pcs/Month" /></p>
-                        <p className=" font-semibold">Screen Printing</p>
+                <div className="card transform hover:bg-pink-500 hover:text-white transition duration-500 hover:scale-105 w-full sm:w-80 md:w-96 bg-base-100 shadow-xl">
+                    <figure><img src={printing} alt="Screen Printing" className="w-full h-48 object-cover" /></figure>
+                    <div className="card-body p-4 md:p-6 lg:p-8">
+                        <p className="card-title text-xl md:text-2xl lg:text-3xl"><Counter number={100000} title=" Pcs/Month" /></p>
+                        <p className="font-semibold text-sm md:text-base lg:text-lg">Screen Printing</p>
                     </div>
                 </div>
             </div>
